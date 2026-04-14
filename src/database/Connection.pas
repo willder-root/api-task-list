7c35a8165b6b59cf0277328497d48e03a561a08f
willder-root <willderaraujo_99_gds@hotmail.com>
2026-04-13 21:13:01 -0300

    Fix: correção para inicialização de banco em ambiente linux

--- src/database/Connection.pas
+++ src/database/Connection.pas
@@ -3,7 +3,10 @@
 interface
 
   uses
-    FireDAC.Comp.Client;
+    FireDAC.Comp.Client,
+    FireDAC.Phys.FB,
+    FireDAC.Phys,
+    system.sysutils;
 
   type
     TConnectionDatabase = class
@@ -11,9 +14,11 @@ interface
     end;
 
 implementation
+uses
+  database.Config;
 
- uses
-    database.Config;
+var
+  DriverLink : TFDPhysFBDriverLink = nil;
 
 class function TConnectionDatabase.New : TFDConnection;
 var
@@ -29,13 +34,28 @@ begin
   end;
 
   Result := TFDConnection.Create(nil);
-  Result.Params.DriverID := 'FB';
-  Result.Params.Database := databasePath;
-  Result.Params.UserName := user;
-  Result.Params.Password := pass;
-  Result.Params.Add('CharacterSet=utf8');
+  Result.Params.Clear;
+  Result.Params.Values['DriverID']    := 'FB';
+  Result.Params.Values['Database']    := databasePath;
+  Result.Params.Values['User_Name']   := user;
+  Result.Params.Values['Password']    := pass;
+  Result.Params.Values['CharacterSet']:= 'UTF8';
+
+
+  // FORA o FireDAC a usar esta lib (caminho completo)
+  Result.Params.Values['VendorLib']   := TDatabaseConfig.Instance.VendorLib;
+
 
 end;
+
+initialization
+  if not Assigned(DriverLink) then
+    DriverLink := TFDPhysFBDriverLink.Create(nil);
+  DriverLink.VendorLib := TDatabaseConfig.Instance.VendorLib;
+
+finalization
+  if Assigned(DriverLink) then
+    FreeAndNil(DriverLink);
 
 end.
 
